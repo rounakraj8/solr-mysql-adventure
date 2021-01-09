d1d00292e038d230ce9f38bb5b992787dcd9b64f
rounakraj8 <rounakraj8@gmail.com>
2021-01-09 20:37:18 +0530

    add batch insertion

--- sample-data-generator/random-data-generator.py
+++ sample-data-generator/random-data-generator.py
@@ -3,19 +3,29 @@ from faker import Faker
 from collections import defaultdict
 from sqlalchemy import create_engine
 
-fake = Faker()
+batch_size=10000
+users_to_insert=10000000
 
-fake_data = defaultdict(list)
+def write():
 
-for _ in range(100000):
-    fake_data["first_name"].append( fake.first_name() )
-    fake_data["last_name"].append( fake.last_name() )
-    fake_data["job"].append( fake.job() )
-    fake_data["dob"].append( fake.date_of_birth() )
-    fake_data["country"].append( fake.country() )
+    fake = Faker()
 
-df_fake_data = pd.DataFrame(fake_data)
+    fake_data = defaultdict(list)
 
-engine = create_engine('mysql://root:root@localhost/solr_test', echo=False)
+    for _ in range(batch_size):
+        fake_data["first_name"].append( fake.first_name() )
+        fake_data["last_name"].append( fake.last_name() )
+        fake_data["job"].append( fake.job() )
+        fake_data["dob"].append( fake.date_of_birth() )
+        fake_data["country"].append( fake.country() )
 
-df_fake_data.to_sql('user', con=engine,index=False, if_exists='append')
+    df_fake_data = pd.DataFrame(fake_data)
+
+    engine = create_engine('mysql://root:root@localhost/solr_test', echo=False)
+
+    df_fake_data.to_sql('user', con=engine,index=False, if_exists='append')
+
+
+for x in range(0, users_to_insert, batch_size):
+    write();
+
